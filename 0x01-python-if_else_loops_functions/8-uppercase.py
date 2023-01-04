1f1f8f4f7a8b8c975d4476bf73cf17c9b3885da0
Vivajayce <joyacha4real@gmail.com>
2023-01-04 11:33:48 -0800

    task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 uppercase = __import__('8-upper.case')
+uppercase("best")
+uppercase("Best School 98 Battery street")
 def uppercase(str):
     tmp = list(str)
     for i in range(len(tmp)):
-        if (ord(tmp[i]) > 96 and ord(tmp[i]) < 123):
+        if (ord(tmp[i]) > 98 and ord(tmp[i]) < 123):
             tmp[i] = chr(ord(tmp[i]) - 32)
             print("{}".format(("").join(tmp)))
