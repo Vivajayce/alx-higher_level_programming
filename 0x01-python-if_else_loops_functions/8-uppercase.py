f051ab3f9154a9984fc86605a2c66ac9ffa653f1
Vivajayce <joyacha4real@gmail.com>
2023-01-04 11:28:39 -0800

    task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-uppercase = __import__('8-uppercase')
-
-uppercase("best")
-uppercase("Best School 98 Battery street")
+uppercase = __import__('8-upper.case')
 def uppercase(str):
     tmp = list(str)
     for i in range(len(tmp)):
