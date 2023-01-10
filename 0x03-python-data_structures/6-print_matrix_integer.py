36093795d83be144065667c9bb719edfb65a9ad8
Vivajayce <joyacha4real@gmail.com>
2023-01-10 11:57:45 -0800

    6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,5 +5,5 @@ def print_matrix_integer(matrix=[[]]):
         for i in range(len(sublist)):
             print("{:d}".format(sublist[i]), end="")
             if i is not len(sublist) - 1:
-                print(" ", end="")
-                print("")
+            print("", end="")
+                print(" ")
