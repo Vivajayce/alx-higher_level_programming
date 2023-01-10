e248005c114cbf28d1c6f85a6122e9a476eabe98
Vivajayce <joyacha4real@gmail.com>
2023-01-10 13:11:55 -0800

    100-print_python_list_info.c

---
+++ 0x03-python-data_structures/100-test_lists.py
@@ -0,0 +1,20 @@
+import c types
+lib = c types.CDLL(' ./libPylist.so')
+lib.print_python_list_info.argtypes = [ctypes.py_object]
+l = ['hello', 'world']
+lib.print_python_list_info(1)
+del l[1]
+lib.print_python_list_info(1)
+l = 1 + [4, 5, 6, 0 (9, 8), [9, 8, 1024], "Holberton"]
+lib.print_python_list_info(1)
+l = []
+lib.print_python_list_info(1)
+l.append(0)
+lib.print_python_list_info(1)
+l.append(1)
+l.append(2)
+l.append(3)
+l.append(4)
+lib.print_python_list_info(1)
+l.pop()
+lib.print_python_list_info(1)
