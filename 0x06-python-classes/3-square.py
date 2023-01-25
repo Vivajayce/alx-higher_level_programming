f2d2bac35f2098dcda92586e0a5b08abfcf4c430
Vivajayce <joyacha4real@gmail.com>
2023-01-25 14:08:40 -0800

    3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 """
 This is the "Square"  module.
+
 This module provides a simple Square class with initialize size.
 Defaults size to 0. Raise error on invalid size inputs.
 Method area returns size of area of the square.
 """
+
+
 class Square:
     """A class that defines a square by size and can compute area"""
     def __init__(self, size=0):
@@ -13,5 +16,6 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
         def area(self):
             return self.__size * self.__size
