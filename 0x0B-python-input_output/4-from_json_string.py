a80d33c6d310e054696e7b19bad54d736eb14fe4
Vivajayce <joyacha4real@gmail.com>
2023-02-08 11:35:26 -0800

    4-from_json_string.py

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,16 +1,8 @@
 #!/usr/bin/python3
+
 import json
 
 
-"""
-from_json_string function module.
-
-Define from_json_string function.
-"""
-
-
 def from_json_string(my_str):
-    """Returns an object made from JSON string.
-    my_str: the string to load from JSON.
-    """
+    """ function that returns an Python data represented by a JSON """
     return json.loads(my_str)
