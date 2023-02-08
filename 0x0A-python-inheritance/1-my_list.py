51cbf3e628b34056e58d85713cdb9032b927ccb3
Vivajayce <joyacha4real@gmail.com>
2023-02-08 02:00:55 -0800

    1-my_list.py

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+MyList class module.
+
+Define MyList class.
+"""
+
+
+"""Define a MyList."""
+
+def print_sorted(self):
+    """Print the list sorted"""
+    print(sorted(self))
