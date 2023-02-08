d92873cd3b29300f6598314100dee840284c5c94
Vivajayce <joyacha4real@gmail.com>
2023-02-08 11:17:24 -0800

    2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
-def read_lines(filename="", nb_lines=0):
-    with open(filename, encoding="utf-8") as fd:
-        if (nb_lines == 0):
-            print(fd.read(), end="")
-        else:
-            for line in fd:
-                if (nb_lines > 0):
-                    print(line, end="")
-                    nb_lines -= 1
+def append_write(filename="", text=""):
+    with open(filename, mode="a", encoding="utf-8") as fd:
+        fd.write(text)
+        return len(text)
