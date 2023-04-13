3c963652b7b5e00e22bce6dd7cdf32e5a4078037
Vivajayce <joyacha4real@gmail.com>
2023-04-13 12:15:21 -0700

    task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const strings = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let i = 0; i < strings.length; i++) {
-  console.log(strings[i]);
+const line = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i < line.length; i++) {
+  console.log(line[i]);
 }
