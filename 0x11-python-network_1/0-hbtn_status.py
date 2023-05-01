f6fe97050f41b30843dd1261659c7521a6c303a4
Vivajayce <joyacha4real@gmail.com>
2023-05-01 11:53:32 -0700

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-""" Fetches basic request with urlib"""
-from urllib import request
-
-with request.urlopen("https://intranet.hbtn.io/status") as response:
-    r = response.read()
-    print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
-          .format(type(r), r, r.decode('utf-8')))
+import urllib.request
+if __name__ == "__main__":
+    print("Body response:")
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as r:
+        content = r.read()
+        print("\t- type: " + str(type(content)))
+        print("\t- content: " + str(content))
+        print("\t- utf8 content: " + content.decode('utf-8'))
